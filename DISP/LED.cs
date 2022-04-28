061dc94abfb4b6ba7c4e3d15215bf6754473aa34
dalleman1 <monglade.dalsgaard@gmail.com>
2022-04-28 11:59:53 +0200

    Finishing changes

--- DISP/LED.cs
+++ DISP/LED.cs
@@ -4,7 +4,6 @@
     {
         public class LED
         {
-            public string color_mode { get; set; }
             public string state { get; set; }
             public int brightness { get; set; }
             public Color color { get; set; }
--- DISP/Program.cs
+++ DISP/Program.cs
@@ -7,6 +7,7 @@ using System.Media;
 using System.Net;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -15,7 +16,6 @@ namespace DISP
     partial class Program
     {
         static MqttClient client;
-
         static bool pillsTaken = false;
         static bool pillsPutBack = false;
         static int numberOfAlotMovements = 0;
@@ -36,37 +36,63 @@ namespace DISP
             client.Subscribe(new string[] { "zigbee2mqtt/0x680ae2fffec0cbbb", "MotionSensor", "WeightSensor" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
             #endregion
 
+            //Mp3 player
+            SoundPlayer player = new SoundPlayer();
+
             //Start by turning off LED
             var led = new LED();
             led.state = "OFF";
             client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
 
+
             var led1 = new LED();
             led1.brightness = 254;
             led1.state = "ON";
-            led1.color_mode = "rgb";
             var col1 = new Color();
             col1.r = 200;
             col1.b = 0;
             col1.g = 0;
             led1.color = col1;
 
+            var firstStopWatch = new Stopwatch();
+
             while (true)
             {
-                if (numberOfAlotMovements == 4)
+                if (numberOfAlotMovements == 3)
                 {
+                    player.SoundLocation = "C:\\Users\\mongl\\source\\repos\\DISP\\DISP\\GodmorgenPIllerWav.wav";
+                    player.Load();
+                    player.Play();
+
+                    firstStopWatch.Start();
                     while (true)
                     {
                         //Person has awoken - sets the LED to blink to indicate the person has to take his medicine.
                         client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led1)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+
+                        Thread.Sleep(5000);
+
+                        col1.r = 0;
                         col1.g = 200;
-                        Thread.Sleep(2);
                         client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led1)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+
+                        Thread.Sleep(5000);
+                        col1.r = 200;
                         col1.g = 0;
+
+                        if (firstStopWatch.Elapsed.Seconds >= 20)
+                        {
+                            player.SoundLocation = "C:\\Users\\mongl\\source\\repos\\DISP\\DISP\\SpisDinePillerWav.wav";
+                            player.Load();
+                            player.Play();
+                        }
 
                         //If he has lifted the pills the program will break out of the blinking mode and set the color to a default white color.
                         if (pillsTaken)
                         {
+                            firstStopWatch.Stop();
                             break;
                         }
                     }
@@ -74,7 +100,6 @@ namespace DISP
                     var led2 = new LED();
                     led2.brightness = 254;
                     led2.state = "ON";
-                    led2.color_mode = "rgb";
                     var col2 = new Color();
                     col2.r = 200;
                     col2.b = 200;
@@ -84,18 +109,19 @@ namespace DISP
                     client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led2)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
 
                     //Checks if the person do not put the pills back.
-                    var stopWatch = new Stopwatch();
-                    stopWatch.Start();
+                    var secondStopWatch = new Stopwatch();
+                    secondStopWatch.Start();
 
                     while (true)
                     {
-                        if (stopWatch.Elapsed.Seconds >= 20)
+                        if (secondStopWatch.Elapsed.Seconds >= 20)
                         {
                             //If a specific amount of time passes we notify the person to put back the pills.
-                            SystemSounds.Exclamation.Play();
-                            //client.Publish("zigbee2mqtt/0x680ae2fffec0cbba/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led2)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                            /*player.SoundLocation = "sæt pillerne tilbage";
+                            player.Load();
+                            player.Play();*/
                         }
-                        
+
                         //If the pills are put back we stop the timing.
                         if (pillsPutBack)
                         {
@@ -104,9 +130,12 @@ namespace DISP
 
                     }
 
-                    stopWatch.Stop();
+                    secondStopWatch.Stop();
 
-                    Console.WriteLine("Out of loop");
+                    //Turn off the alarm
+                    client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+                    
+                    
                     Console.ReadLine();
                 }
 
@@ -139,7 +168,7 @@ namespace DISP
 
         static void OnMotionEvent(string message)
         {
-           
+
             if (!pillsTaken)
             {
                 switch (message)
@@ -154,32 +183,15 @@ namespace DISP
                         {
                             numberOfAlotMovements = 0;
                         }
-                        
+
                         previousMovement = message;
 
                         break;
                     case "No":
                         previousMovement = message;
-                        /*
-                        var led = new LED();
-                        led.state = "OFF";
-                        client.Publish("zigbee2mqtt/0x680ae2fffec0cbba/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                        */
                         break;
                     case "Some":
                         previousMovement = message;
-                        /*
-                        var led2 = new LED();
-                        led2.brightness = 50;
-                        led2.state = "ON";
-                        led2.color_mode = "rgb";
-                        var col2 = new Color();
-                        col2.r = 0;
-                        col2.b = 200;
-                        col2.g = 200;
-                        led2.color = col2;
-                        client.Publish("zigbee2mqtt/0x680ae2fffec0cbba/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led2)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                        */
                         break;
                     default:
                         break;
@@ -189,43 +201,16 @@ namespace DISP
 
         static void OnWeightEvent(string message)
         {
-            
+
             if (message == "On")
             {
-                var led = new LED();
-                led.brightness = 254;
-                led.state = "ON";
-                led.color_mode = "rgb";
-                var col = new Color();
-                col.r = 200;
-                col.b = 0;
-                col.g = 200;
-                led.color = col;
-
                 pillsPutBack = true;
-
-                client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
             }
 
             if (message == "Off")
             {
-                var led2 = new LED();
-                led2.brightness = 254;
-                led2.state = "ON";
-                led2.color_mode = "rgb";
-                var col2 = new Color();
-                col2.r = 200;
-                col2.b = 200;
-                col2.g = 200;
-                led2.color = col2;
-
                 pillsTaken = true;
-
-                client.Publish(zigbeeLedAddress + "/set", Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(led2)), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
             }
-
-            
-
         }
     }
 }
